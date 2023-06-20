f33f1497a3452a5bef56e988edfe543503d53d41
Vitorio Suarez <vitoriosuarez.06@gmail.com>
2023-06-20 09:05:12 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+
+
+modeloJson.map((item, index) => {
+    let modeloItem = c('.models .modelo-item').cloneNode(true);
+
+    modeloItem.setAttribute('data-key', index);
+
+    modeloItem.querySelector('.modelo-item-img img').src = item.img;
+    modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
+    modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
+    modeloItem.querySelector('.modelo-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+
+    modeloItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let key = e.target.closest('.modelo-item').getAttribute('data-key');
+
+        
+        c(".modeloBig img").src = modeloJson[key].img
+        c(".modeloInfo h1").innerHTML = modeloJson[key].name
+        c(".modeloInfo .modeloInfo-desc").innerHTML = modeloJson[key].description
+        c(".modeloInfo .modeloInfo-pricearea .modeloInfo-actualPrice").innerHTML = `R$ ${modeloJson[key].price.toFixed(2)}`;
+
+        cs(".modeloInfo-size").forEach((size, sizeindex) =>{
+            size.querySelector('span').innerHTML = modeloJson[key].sizes[sizeindex];
+            
+        })
+
+
+        c('.modeloWindowArea').style.opacity = 0;
+        c('.modeloWindowArea').style.display = 'flex';
+
+        setTimeout(() => {
+            c('.modeloWindowArea').style.opacity = 1;
+        }, 100);
+        });
+
+    c('.modelo-area').append(modeloItem);
+});
+
